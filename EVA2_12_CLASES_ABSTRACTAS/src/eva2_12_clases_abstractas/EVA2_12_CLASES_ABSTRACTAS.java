8461b4f0c52c1f49f06491755c6c35c2a01574f8
jaquiflan <124392609+jaquiflan@users.noreply.github.com>
2023-05-14 22:21:22 -0600

    Add files via upload

---
+++ EVA2_12_CLASES_ABSTRACTAS/src/eva2_12_clases_abstractas/EVA2_12_CLASES_ABSTRACTAS.java
@@ -0,0 +1,22 @@
+package eva2_12_clases_abstractas;
+
+/**
+ *
+ * @author campa
+ */
+public class EVA2_12_CLASES_ABSTRACTAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PruebaAbstracta pruebaAbs = new PruebaAbstracta();
+    }   //No se puede crear un objeto de una clase abstracta
+    
+}
+
+class prueba{
+}
+
+abstract class PruebaAbstracta{
+}
